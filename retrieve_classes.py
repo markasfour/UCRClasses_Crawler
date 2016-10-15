1b39141ca83d47f1d74f2f5b0baa25f1da330d47
Mark Asfour <mj4sparrow@gmail.com>
2016-10-14 18:31:48 -0700

    fixed firebase data pushing

--- retrieve_classes.py
+++ retrieve_classes.py
@@ -23,7 +23,8 @@ class ClassSearch():
 
     def __init__(self):
         self.driver = webdriver.Chrome()
-        self.subjects = []
+        self.quarter = ''
+	self.subjects = []
         self.num_pages = 0
 
 
@@ -38,6 +39,17 @@ class ClassSearch():
       
     def click_search(self):
         search = self.driver.find_element_by_name("btn_search").click()
+
+
+    def get_quarter(self):
+        quarters = []
+        quarter_elements = self.driver.find_element_by_name("drp_term")
+        all_options = quarter_elements.find_elements_by_tag_name("option")        
+        for option in all_options:
+            if option.get_attribute("value") == "" :
+                continue
+            quarters.append(option.get_attribute("value"))
+        self.quarter = quarters[0]
 
 
     def get_subject_options(self):
@@ -74,9 +86,9 @@ class ClassSearch():
         else:
           self.num_pages = self.num_pages + 1
         print "going to page %s" %self.num_pages
-        
-    #---MAIN FUNCTION TO RETRIEVE TEXT OF TABLE PAGES---#    
-    def getinfo(self):
+    
+    
+    def getinfo(self, subject):
       for x in range (02, 12):
         if (x < 10):
             popup = "javascript:__doPostBack('grid_students$ctl0"+str(x)+"$lnkbtn_courseTitle','')"
@@ -107,11 +119,11 @@ class ClassSearch():
         table.append(self.driver.find_element_by_xpath('//*[@id="lbl_notes"]').text)
         #each table will have 0 to 19 indexes
         
-        data = {'CourseTitle': table[0], 'name': table[2]}
-        
+        data = {'Subject': subject, 'CourseTitle': table[0], 'CourseNum': table[1], 'CallNo': table[2], 'Instructor': table[3], 'Units': table[4], 'MaxEnrollment': table[5], 'Lec_Dis': table[6], 'Days': table[7], 'Time': table[8], 'RoomAbrv': table[9], 'BuildingName': table[10], 'AvailableSeats': table[11], 'WaitListMax': table[12], 'NumberonWaitList': table[13], 'Co-requisites': table[14], 'Prerequisites': table[15], 'Restrictions': table[16], 'FinalExamDate': table[17], 'FinalExamTime': table[18], 'CatalogDescription': table[19]}
+         
 #        data = {'CourseTitle': table[0], 'CourseNumber': table[1], 'CallNumber': table[2], 'Instructor': table[3], 'Units': table[4], 'MaxEnrollment': table[5], 'Lec/Dis': table[6], 'Days': table[7], 'Time': table[8], 'RoomAbrv': table[9], 'BuildingName': table[10], 'AvailableSeats': table[11], 'WaitListMax': table[12], 'NumberonWaitList': table[13], 'Co-requisites': table[14], 'Prerequisites': table[15], 'Restrictions': table[16], 'FinalExamDate': table[17], 'FinalExamTime': table[18], 'CatalogDescription': table[19]}
         
-        result = requests.post(firebase_url + '/' + table[0] + '/' + 'course.json', data=json.dumps(data))
+        result = requests.patch(firebase_url + '/' + self.quarter + '/' + subject + '/' + table[2] + '.json', data=json.dumps(data))
 #        result = requests.post(firebase_url + '/' + table[0] + '/course.json', data=json.dumps(data))
         print 'Record inserted. Result Code = ' + str(result.status_code) + ',' + result.text
 
@@ -186,7 +198,7 @@ class ClassSearch():
             self.click_search()
             time.sleep(3)  ###IMPROVABLE
             
-            self.getinfo()
+            self.getinfo(subject)
             print('\n')
           #---RETRIEVE DATA FROM ALL PAGES FOR THIS SUBJECT---
             self.get_next_page()
@@ -196,9 +208,10 @@ class ClassSearch():
               time.sleep(2)  ###IMPROVABLE
               self.get_next_page()
               
-              self.getinfo()
+              self.getinfo(subject)
               print('\n')
           #---RETRIEVE DATA FROM ALL PAGES FOR THIS SUBJECT---
+
 
 if __name__ == "__main__":
     firebase = firebase.FirebaseApplication('https://cs180-bf6af.firebaseio.com/', authentication=None)
@@ -208,6 +221,7 @@ if __name__ == "__main__":
 
     retriever = ClassSearch()
     retriever.start_connection()
+    retriever.get_quarter()
     retriever.get_subject_options()
     retriever.iterate_subject_options()
     retriever.end_connection()
